1d72b7b4f3495d8ac3e9f77a7cb929a4b546d513
Alex <ruimf3@outlook.com>
2026-07-01 16:32:15 -0200

    feat: capa do eBook "How Paraguay Broke Germany"
    Adiciona imagem_url do produto a partir do ficheiro hospedado no S3 do Hotmart.

---
+++ src/main/resources/db/migration/USER/V20260701_02__how_paraguay_broke_germany_imagem.sql
@@ -0,0 +1,3 @@
+UPDATE copa_produto
+SET imagem_url = 'https://hotmart.s3.amazonaws.com/product_pictures/98bd3909-8d16-4df7-9823-396551f38b55/cover_portrait.png'
+WHERE slug = 'how-paraguay-broke-germany';
